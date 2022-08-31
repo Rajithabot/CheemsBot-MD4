4455ba84ceb8ed64bd3b4f5b10fc32c53d8ff07c
Rajithabot <112554663+Rajithabot@users.noreply.github.com>
2022-08-31 04:48:32 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -63,7 +63,7 @@ global.location = "India, Mizoram, Aizawl" //ur location
 //bot bomdy 
 global.owner = ['94742944882']
 global.ownertag = '94742944882' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.botname = 'roshanbot' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
